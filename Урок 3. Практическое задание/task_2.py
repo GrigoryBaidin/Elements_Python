951a8c04b178cac4fcb51c6042a6354c603f5bae
Отец <grigory7123@gmail.com>
2022-04-04 18:18:36 +0300

    task_1 completed

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,14 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+def func_user():
+    name = input('name')
+    surname = input('surname')
+    year_birth = int(input('year of birth'))
+    location = input('location')
+    email = input('email')
+    telefon = int(input('telefon'))
+    return f'{name} {surname} {year_birth} года рождения,проживает в г.{location}, email:{email}, телефон:{telefon}'
+
+
+print(func_user())
