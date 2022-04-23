5758497234a1b8bd07e74e0b9390ebbe777949fc
Отец <grigory7123@gmail.com>
2022-04-23 16:35:56 +0300

    lesson_6

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,34 @@
 8 10 12
 14 16 18
 """
+class Matrix:
+    def __init__(self, *args):
+        # self.matr = [new_lst, list_2]
+        self.new_lst = list(args)
+
+    def __str__(self):
+        matr='\n'.join(map(str, self.new_lst))
+        '''matr -> [[1,2,3],
+                [4,5,6],
+                [7,8,9]]'''
+        matr=matr.replace(',','').replace('[','').replace(']','')
+        '''matr-> 1 2 3
+                  4 5 6
+                  7 8 9'''
+        return matr
+    def __add__(self, other):
+        new_sum=[]
+        line_sum=[]
+        for i in range(len(self.new_lst)):
+            for el in range(len(self.new_lst[i])):
+                line_sum.append(self.new_lst[i][el] +other.new_lst[i][el])
+            new_sum.append(line_sum)
+            line_sum=[]
+        return Matrix('\n'.join(map(str, new_sum)))
+Matr_Obj=Matrix([1,2,3],[4,5,6],[7,8,9])
+print(Matr_Obj)
+print()
+Matr_Obj_1=Matrix([1,2,3],[4,5,6],[7,8,9])
+print(Matr_Obj_1)
+print()
+print(f'Sum Matrix:\n{Matr_Obj+Matr_Obj_1 }')
